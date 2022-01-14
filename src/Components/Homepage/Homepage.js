54280cffb822fe2854017757c3df6fbd706e9883
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-14 20:21:55 +0530

    initial commit

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
+class Homepage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Homepage;
